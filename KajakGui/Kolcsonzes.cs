8123837b9ea63b20fc49c5a580e7e6619c748d0f
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-02-07 11:24:15 +0100

    Add project files.

---
+++ KajakGui/Kolcsonzes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KajakGui
+{
+    class Kolcsonzes
+    {
+        public Kolcsonzes(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+
+            if (Nev.Contains(','))
+            {
+                var nevekCsoport = Nev.Split(',');
+                var temping = nevekCsoport[0].Trim();
+                nevekCsoport[0] = nevekCsoport[1].Trim();
+                nevekCsoport[1] = temping;
+
+                Nev = $"{nevekCsoport[0]}, {nevekCsoport[1]}";
+            }
+
+
+            Id = int.Parse(v[1]);
+            HajoTipus = v[2];
+            SzemelyekSzama = int.Parse(v[3]);
+            this.elvitelOraja = int.Parse(v[4]);
+            this.elvitelPerce = int.Parse(v[5]);
+            VisszahozatalOraja = int.Parse(v[6]);
+            VisszahozatalPerce = int.Parse(v[7]);
+
+
+
+
+
+
+
+
+        }
+
+        public bool VizennVanE(int ora, int perc)
+        {
+
+            int elvitelIdoPercben = elvitelOraja * 60 + elvitelPerce;
+            int visszahozatalIdoPercben = VisszahozatalOraja * 60 + VisszahozatalPerce;
+            int keresettIdoPercben = ora * 60 + perc;
+            return keresettIdoPercben >= elvitelIdoPercben + 1 && keresettIdoPercben <= visszahozatalIdoPercben;
+        }
+        public int perceAlakitas() {
+
+            int allelvitel = elvitelOraja * 60 + elvitelPerce;
+            int allvisszahozatal = VisszahozatalOraja * 60 + VisszahozatalPerce;
+
+            int talalat = allvisszahozatal - allelvitel;
+            return talalat;
+        }
+        public int mennyiFelora() {
+
+            int perc = perceAlakitas();
+            int talalat = (int)Math.Ceiling((double)perc / 30);
+            return talalat;
+        }
+
+      
+
+        public string Nev { get; set; }
+        public int Id { get; set; }
+        public string HajoTipus { get; set; }
+        public int SzemelyekSzama { get; set; }
+        public int elvitelOraja { get; set; }
+        public int elvitelPerce { get; set; }
+        public int VisszahozatalOraja { get; set; }
+        public int VisszahozatalPerce { get; set; }
+
+        public override string ToString()
+        {
+            int percalakitas = perceAlakitas();
+            int ennyifelora = mennyiFelora();
+            return $"{Id}\t{percalakitas} perc \t{ennyifelora} fél óra kezdődött meg ";
+        }
+
+    }
+}
---
+++ KajakGui/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KajakGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Kolcsonzes> kolcsonzes = new List<Kolcsonzes>();
+        public MainWindow()
+        {
+
+
+
+
+
+            InitializeComponent();
+            beolvasas();
+            foreach (var item in kolcsonzes)
+            {
+                alapGrid.Items.Add(item);
+            }
+
+            for (int i = 0; i < 24; i++)
+            {
+                Ora.Items.Add(i);
+            }
+            for (int i = 0; i < 59; i++)
+            {
+                Perc.Items.Add(i);
+            }
+
+            foreach (var item in kolcsonzes)
+            {
+                Listbox.Items.Add(item.ToString());
+            }
+
+
+            #region comboboxFeltoltes
+            for (int i = 1; i < 26; i++)
+            {
+                HajodIdCb.Items.Add(i);
+            }
+            var hajotipusok = kolcsonzes.Select(x=>x.HajoTipus).Distinct().ToList();
+            foreach (var item in hajotipusok)
+            {
+                HajoTipusCb.Items.Add(item);
+            }
+            var szemelyekSzama = kolcsonzes.Select(x => x.SzemelyekSzama).Distinct().ToList();
+            foreach (var item in szemelyekSzama)
+            {
+                HajoSzemelyekSzamaCB.Items.Add(item);
+            }
+            #endregion
+
+
+
+        }
+
+
+        public bool f15(int id, string hajotipusa, int szemelyekszama)
+        {
+            foreach (var item in kolcsonzes)
+            {
+                if (item.Id == id || hajotipusa == hajotipusa || szemelyekszama == szemelyekszama)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+          
+            return true;
+        }
+        public void beolvasas()
+        {
+            
+            
+            using StreamReader sr = new($"../../../src/kolcsonzes.txt", Encoding.UTF8);
+            _= sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                kolcsonzes.Add(new Kolcsonzes(sr.ReadLine()));
+            }
+
+        
+        }
+
+        private void Perc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            {
+                if (Ora.SelectedItem == null || Perc.SelectedItem == null)
+                {
+                    MessageBox.Show("Válassz ki egy időpontot!");
+                    return;
+                }
+                int ora = (int)Ora.SelectedItem;
+                int perc = (int)Perc.SelectedItem;
+                var vizenvan = kolcsonzes.Where(k => k.VizennVanE(ora, perc)).ToList();
+
+                if (vizenvan.Count == 0)
+                {
+                    MessageBox.Show("Nincs a megadott időpontban vízen lévő hajó.");
+                }
+                else
+                {
+                    foreach (var item in vizenvan)
+                    {
+                        szamoloGrid.Items.Add(item);
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void napibevetelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var napi = kolcsonzes.Sum(x => x.mennyiFelora() * 1500);
+            eredmenyLbl.Content = $"a napi bevétel {napi} ft";
+        }
+
+        private void f16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
